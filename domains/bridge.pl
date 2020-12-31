f090cb1d85545b8eab368560e5d34c583221e573
Felix Lindner <felix@existenzquantor.info>
2020-12-31 12:43:25 +0100

    .

--- domains/bridge.pl
+++ domains/bridge.pl
@@ -1,10 +1,10 @@
+n([not(man_on_track), not(dead)]).
+
 effect(push, [], [man_on_track]).
 effect(0, [not(man_on_track)], [dead]).
 
-init([not(man_on_track), not(dead)]).
-plan(push).
 goal([not(dead)]).
-
+plan(push).
 
 utility(man_on_track, -1).
 utility(dead, -1).
